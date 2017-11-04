301b4edfa988d9e41212be75ab55a9da1e8e5e31
nickg902 <github@cipherspace.ca>
2017-11-03 22:55:23 -0300

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// Smooth Scroll
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+
+    return false;
+});
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += "responsive";
-    } else {
-        x.className = "topnav";
-    }
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
